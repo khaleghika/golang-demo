084a6b11e55e25a8bbd132f12d5acc625b98f3a6
khaleghika <khaleghika@gmail.com>
2021-10-31 22:44:50 +0330

    Update main.go

--- main.go
+++ main.go
@@ -1,18 +1,112 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const botToken = "2089411348:AAHdzyPFMTPzhhLIwX8vCr4E2E6P950q3b4"
 
 func main() {
-	r := gin.Default()
-	r.GET("", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello from zeet",
-		})
-	})
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	err := http.ListenAndServe(":8080", http.HandlerFunc(webHookHandler))
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+}
+
+type webHookReqBody struct {
+	Message struct {
+		Text string `json:"text"`
+		Chat struct {
+			ID int64 `json:"id"`
+		} `json:"chat"`
+	} `json:"message"`
+}
+
+func webHookHandler(rw http.ResponseWriter, req *http.Request) {
+
+	// Create our web hook request body type instance
+	body := &webHookReqBody{}
+
+	// Decodes the incoming request into our cutom webhookreqbody type
+	if err := json.NewDecoder(req.Body).Decode(body); err != nil {
+		log.Printf("An error occured (webHookHandler)")
+		log.Panic(err)
+		return
+	}
+
+	// If the command /joke is recieved call the sendReply function
+	if strings.ToLower(body.Message.Text) == "/joke" {
+		err := sendReply(body.Message.Chat.ID)
+		if err != nil {
+			log.Panic(err)
+			return
+		}
+	}
+}
+
+func sendReply(chatID int64) error {
+	fmt.Println("sendReply called")
+
+	// calls the joke fetcher fucntion and gets a random joke from the API
+	text := "Salam"
+
+	//Creates an instance of our custom sendMessageReqBody Type
+	reqBody := &sendMessageReqBody{
+		ChatID: chatID,
+		Text:   text,
+	}
+
+	// Convert our custom type into json format
+	reqBytes, err := json.Marshal(reqBody)
+
+	if err != nil {
+		return err
+	}
+
+	// Make a request to send our message using the POST method to the telegram bot API
+	resp, err := http.Post(
+		"https://api.telegram.org/bot"+botToken+"/"+"sendMessage",
+		"application/json",
+		bytes.NewBuffer(reqBytes),
+	)
+
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return errors.New("unexpected status" + resp.Status)
+	}
+
+	return err
+}
+
+func jokeFetcher() (string, error) {
+	resp, err := http.Get("http://api.icndb.com/jokes/random")
+	c := &joke{}
+	if err != nil {
+		return "", err
+	}
+	err = json.NewDecoder(resp.Body).Decode(c)
+	return c.Value.Joke, err
+}
+
+type joke struct {
+	Value struct {
+		Joke string `json:"joke"`
+	} `json:"value"`
+}
+
+type sendMessageReqBody struct {
+	ChatID int64  `json:"chat_id"`
+	Text   string `json:"text"`
 }
